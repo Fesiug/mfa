f92813cfe902378fb0529199c490e8e6bf259f58
Fesiug <publicfesiug@outlook.com>
2022-09-07 20:18:56 -0400

    Create mfa_w2_submachinepistol.lua

---
+++ lua/weapons/mfa_w2_submachinepistol.lua
@@ -0,0 +1,98 @@
+
+-- Fesiug, 2022
+
+SWEP.Base					= "mfa_base_wep"
+SWEP.Spawnable				= true
+
+SWEP.Slot					= 3
+SWEP.Category				= "MFA"	
+SWEP.PrintName				= "Submachine Pistol"
+SWEP.Trivia = {
+	Category = "Submachine Gun",
+	["Real Name"] = "MP9",
+}
+
+SWEP.UseHands				= true
+SWEP.ViewModel				= "models/mfa/weapons/mp9n.mdl"
+SWEP.WorldModel				= "models/mfa/weapons/mp9n.mdl"
+SWEP.ViewModelFOV			= 70
+
+SWEP.Primary.ClipSize		= 30
+SWEP.Primary.Ammo			= "pistol"
+SWEP.Primary.Sound			= ")weapons/iw3/mp5/fire.wav"
+
+SWEP.DamageNear				= 24
+SWEP.DamageFar				= 16
+SWEP.RangeNear				= 20
+SWEP.RangeFar				= 40
+
+--
+-- Recoil
+--
+SWEP.RecoilUp							= 1.7 -- degrees punched
+SWEP.RecoilUpDecay						= 25 -- how much recoil to remove per second
+SWEP.RecoilSide							= 0.6 -- degrees punched, in either direction (-100% to 100%)
+SWEP.RecoilSideDecay					= 25 -- how much recoil to remove per second
+SWEP.RecoilUpDrift						= 0.5 -- how much will be smooth recoil
+SWEP.RecoilSideDrift					= 0.5 -- how much will be smooth recoil
+SWEP.RecoilFlipChance					= ( 2 / 3 ) -- chance to flip recoil direction
+SWEP.RecoilADSMult						= ( 3 / 4 ) -- multiply shot recoil by this amount when ads'd
+
+SWEP.Dispersion				= 0.8
+SWEP.Dispersion_Move		= 2 -- at 200 hu/s
+SWEP.Dispersion_Air			= 2
+SWEP.Dispersion_Crouch		= ( 2 / 3 )
+SWEP.Dispersion_Sights		= ( 1 / 3 )
+
+SWEP.Dispersion_FireShoot	= 0.4
+SWEP.Dispersion_FireDecay	= 3
+
+
+SWEP.Firemodes = {
+	{
+		Count = math.huge,
+		Delay = 0.075,
+	},
+	{
+		Count = 1,
+		Delay = 0.075,
+	}
+}
+
+SWEP.ActivePos = {
+	Pos = Vector(-0, 0, -0.4),
+	Ang = Angle(),
+}
+
+SWEP.IronsightPos = {
+	Pos = Vector(-2.31, -2, 0.45),
+	Ang = Angle(0, 0, 0),
+}
+
+SWEP.Animations = {
+	["idle"] = {
+		Source = "base_idle",
+	},
+	["draw"] = {
+		Source = "base_draw",
+	},
+	["holster"] = {
+		Source = "base_holster",
+	},
+	["fire"] = {
+		Source = "base_fire",
+	},
+	["fire_ads"] = {
+		Source = "iron_fire",
+	},
+	["reload"] = {
+		Source = "base_reload",
+		Time = 2.8,
+		LoadIn = 2.0,
+	},
+	["reload_empty"] = {
+		Source = "base_reloadempty",
+		Time = 3.7,
+		LoadIn = 3.1,
+	}
+}
